0ef234cb9f6737db7aeb991523928f8b0b6eab32
liujp2015 <392303104@qq.com>
2025-10-20 14:38:52 +0800

    adding create_journal_entity

--- anchor/programs/crud/src/lib.rs
+++ anchor/programs/crud/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::result_large_err)]
 
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::sysvar::instructions};
 
 declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 
@@ -8,7 +8,12 @@ declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 pub mod crud {
     use super::*;
 
-    pub fn create_journal_entity(ctx:Context<CreateEntity>) -> Result<()>{
+    pub fn create_journal_entity(ctx:Context<CreateEntity>,title:String,message:String) -> Result<()>{
+        let journal_entity = &mut ctx.accounts.journal_entity; 
+        journal_entity.owner = *ctx.accounts.owner.key;
+        journal_entity.message = message;
+        journal_entity.title = title;
+
         Ok(())
     }
 
@@ -16,14 +21,25 @@ pub mod crud {
 }
 
 #[derive(Accounts)]
+#[instruction(title:String)]
 pub struct CreateEntity<'info>{
+
+    #[account(
+    init,
+    seeds = [title.as_bytes(),owner.key().as_ref()],
+    bump,
+    space = 8+JournalEntityState::INIT_SPACE,
+    payer = owner,
+    )]
+    pub journal_entity:Account<'info,JournalEntityState>,
+    #[account(mut)]
     pub owner:Signer<'info>,
-    pub system_program:Program<'info,System>
+    pub system_program:Program<'info,System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct journalEntityState{
+pub struct JournalEntityState{
     pub owner:Pubkey,
     #[max_len(50)]
     pub title:String,
